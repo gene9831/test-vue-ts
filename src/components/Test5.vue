16dcae775f2f621c31b70d3eb26220ab6a198e3b
gene9831 <pacify.98@gmail.com>
2024-08-26 17:59:17 +0800

    add Test5

---
+++ src/components/Test5.vue
@@ -0,0 +1,105 @@
+<!-- 防腐冰柜看板 -->
+<template>
+  <h2 style="text-align: center">火化炉看板</h2>
+
+  <div style="margin: 20px">
+    <div
+      style="
+        margin-bottom: 20px;
+        font-size: small;
+        color: #909399;
+        display: flex;
+        gap: 20px;
+        align-items: center;
+      "
+    >
+      <div>
+        <span style="margin-right: 10px">类型</span>
+        <el-select placeholder="类型筛选" style="width: 240px"></el-select>
+      </div>
+      <div>
+        <span style="margin-right: 10px">状态</span>
+        <el-select placeholder="状态筛选" style="width: 240px"></el-select>
+      </div>
+    </div>
+    <el-row :gutter="20" class="row row23">
+      <el-col :span="3" v-for="i in 16">
+        <div
+          style="
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            width: 100%;
+            height: 100%;
+          "
+        >
+          <div
+            style="width: 100%; height: 100%; font-size: small"
+            :class="[
+              'cell-hgrtft',
+              {
+                finish: getType(i) === 'info',
+                using: getType(i) === 'primary',
+                low: getType(i) === 'warning',
+              },
+            ]"
+          >
+            <div>火化炉{{ i }}</div>
+            <div v-if="isUsing(i)">编号：10000</div>
+            <div v-if="isUsing(i)">死者姓名：张三</div>
+            <div v-if="getType(i) === 'primary'">状态：使用中</div>
+            <div v-if="getType(i) === 'warning'">状态：使用中(油量低)</div>
+            <div>温度：xxx</div>
+            <div>油量：xxx</div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const using = [3, 4, 5];
+const status2 = [10, 15];
+const status3 = [8];
+const getType = (i: number) => {
+  if (using.includes(i)) {
+    return "primary";
+  }
+
+  if (status2.includes(i)) {
+    return "warning";
+  }
+
+  if (status3.includes(i)) {
+    return "warning2";
+  }
+
+  return "info";
+};
+
+const isUsing = (i: number) =>
+  getType(i) === "primary" || getType(i) === "warning";
+</script>
+
+<style>
+.row > div {
+  border: 1px solid #e2e2e2;
+  text-align: center;
+}
+.row23 > div {
+  display: flex !important;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  padding: 0 !important;
+}
+.a-button {
+  width: 100% !important;
+  height: 100% !important;
+  margin: 0 !important;
+}
+.cell-hgrtft.low {
+  background-color: red;
+}
+</style>
